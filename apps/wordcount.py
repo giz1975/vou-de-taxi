9dd12c84acc11223dae45e7c6acdf007287494b1
giz1975 <gabriel@izar.com.br>
2026-01-19 18:39:11 -0300

    chore: add apps directory

---
+++ apps/wordcount.py
@@ -0,0 +1,28 @@
+﻿from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+
+spark = (SparkSession.builder
+         .appName("WordCount-PT")
+         .getOrCreate())
+
+text = [
+    "Leninha aguenta, mas não perdoa.",
+    "Spark distribui trabalho; desigualdade distribui conta.",
+    "PySpark é conversa séria com dado grande."
+]
+
+df = spark.createDataFrame(text, "string").toDF("line")
+
+words = (df
+         .select(F.explode(F.split(F.lower(F.col("line")), r"[^\p{L}]+")).alias("word"))
+         .where(F.col("word") != ""))
+
+counts = (words
+          .groupBy("word")
+          .count()
+          .orderBy(F.desc("count"), F.asc("word")))
+
+counts.show(50, truncate=False)
+spark.stop()
+
+
